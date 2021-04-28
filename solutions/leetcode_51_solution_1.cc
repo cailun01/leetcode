f259828cf8796a3e908263c1018086ffb28ffc7b
cailun01 <1354213521@qq.com>
2021-04-28 20:52:11 +0800

    add some notes to leetcode_51

--- solutions/leetcode_51_solution_1.cc
+++ solutions/leetcode_51_solution_1.cc
@@ -6,20 +6,20 @@ public:
 
     bool is_valid(vector<string>& board, int row, int col) {
         int n = board.size();
-
+        // 检查列是否有皇后互相冲突
         for (int i = 0; i < n; ++i) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
-
+        // 检查左上方是否有皇后互相冲突
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0;
                  --i, --j) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
-
+        // 检查右上方是否有皇后互相冲突
         for (int i = row - 1, j = col + 1; i >= 0 && j < n;
                 --i, ++j) {
             if (board[i][j] == 'Q') {
@@ -36,12 +36,15 @@ public:
         }
         int n = board[row].size();
         for (int col = 0; col < n; ++col) {
+            // 排除不合法选择
             if (!is_valid(board, row, col)) {
                 continue;
             }
-
+            // 做选择
             board[row][col] = 'Q';
+            // 进入下一行决策
             backtrack(board, row + 1);
+            // 撤销选择
             board[row][col] = '.';
         }
     }
