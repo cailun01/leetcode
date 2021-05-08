5792b0a1ad7f249edf1ae70c755f37f388dd0bb3
cailun01 <1354213521@qq.com>
2021-05-08 09:05:32 +0800

    change leetcode 114 comments

--- solutions/leetcode_114_solution_1.cc
+++ solutions/leetcode_114_solution_1.cc
@@ -15,6 +15,10 @@
 
 输入：root = [0]
 输出：[0]
+
+题解：
+https://labuladong.github.io/algo/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%B3%BB%E5%88%971.html
+
 */
 
 class Solution {
@@ -36,6 +40,3 @@ public:
         p->right = right_temp;
     }
 };
-
-// 题解：
-https://labuladong.github.io/algo/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%B3%BB%E5%88%971.html
