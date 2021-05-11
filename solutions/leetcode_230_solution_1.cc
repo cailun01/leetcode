dc1e2ac8530d80700abf55ead611111e33448266
cailun01 <1354213521@qq.com>
2021-05-11 23:07:30 +0800

    add notes to leetcode 230

--- solutions/leetcode_230_solution_1.cc
+++ solutions/leetcode_230_solution_1.cc
@@ -1,6 +1,12 @@
+/*
+给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+*/
+
 class Solution {
 private:
+    // 记录当前元素的排名
     int num = 0;
+    // 记录结果
     int res = 0;;
 public:
     void search(TreeNode* root, int k) {
@@ -10,8 +16,10 @@ public:
         if (root->left) {
             search(root->left, k);
         }
+        // 中序遍历
         ++num;
         if (num == k) {
+            // 找到第 k 小的元素
             res = root->val;
         } else if (num > k) {
             return;
