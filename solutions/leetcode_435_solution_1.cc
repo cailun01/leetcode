345fd15b886058d47e1071c1345430d847ac927b
cailun01 <1354213521@qq.com>
2021-05-11 10:39:12 +0800

    interval overlap

--- solutions/leetcode_435_solution_1.cc
+++ solutions/leetcode_435_solution_1.cc
@@ -1,10 +1,34 @@
+/*
+题目：
+给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+
+注意:
+	1. 可以认为区间的终点总是大于它的起点。
+	2. 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+	
+示例 1:
+输入: [ [1,2], [2,3], [3,4], [1,3] ]
+输出: 1
+解释: 移除 [1,3] 后，剩下的区间没有重叠。
+
+示例 2:
+输入: [ [1,2], [1,2], [1,2] ]
+输出: 2
+解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+
+示例 3:
+输入: [ [1,2], [2,3] ]
+输出: 0
+解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+*/
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.size() == 0 || intervals.size() == 1) {
             return 0;
         }
-
+        // intervals中都是vector<int>，形式为 [ [start0, end0], [start1, end1] ... ]
+        // 让intervals中的vector<int>按照结尾end升序
         sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b){
             return a[1] < b[1];
         });
@@ -15,8 +39,11 @@ public:
             const vector<int>& interval = intervals[i];
             int start = interval[0];
             if (start >= x_end) {
+                // 当前interval与上一个interval不重叠
+                // 更新x_end为当前interval的end
                 x_end = interval[1];
             } else {
+                // start < x_end，需要该区间
                 ++count;
             }
         }
--- solutions/leetcode_438_solution_1.cc
+++ solutions/leetcode_438_solution_1.cc
@@ -1,3 +1,28 @@
+/*
+给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+说明：
+	• 字母异位词指字母相同，但排列不同的字符串。
+	• 不考虑答案输出的顺序。
+示例 1:
+输入:
+s: "cbaebabacd" p: "abc"
+输出:
+[0, 6]
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+ 示例 2:
+输入:
+s: "abab" p: "ab"
+输出:
+[0, 1, 2]
+解释:
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的字母异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的字母异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的字母异位词。
+
+*/
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
@@ -5,7 +30,7 @@ public:
         // valid变量表示窗口中满足need条件的字符个数
         int valid = 0;
         // need记录p中的字符出现的次数
-        // window记录窗口中的字符出现的次数
+        // window记录窗口中的需要的字符出现的次数
         unordered_map<char, int> need, window;
         vector<int> res;
         for (auto c : p) {
