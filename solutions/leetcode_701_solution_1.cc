92284f14510936a01e966dc41d77af23b227dcee
cailun01 <1354213521@qq.com>
2021-05-11 23:32:00 +0800

    add notes to leetcode 701

--- solutions/leetcode_701_solution_1.cc
+++ solutions/leetcode_701_solution_1.cc
@@ -4,9 +4,11 @@ public:
         if (!root) {
             return new TreeNode(val);
         }
+        // 目标值val小于root的值，插入到左子树
         if (root->val > val) {
             root->left = insertIntoBST(root->left, val);
         } 
+        // 目标值val大于root的值，插入到右子树
         if (root->val < val) {
             root->right = insertIntoBST(root->right, val);
         }
