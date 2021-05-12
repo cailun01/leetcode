4098040b5127f1137d6c41afce5483ac057772cd
cailun01 <1354213521@qq.com>
2021-05-12 18:06:13 +0800

    add notes to leetcode 106

--- solutions/leetcode_106_solution_1.cc
+++ solutions/leetcode_106_solution_1.cc
@@ -13,24 +13,24 @@ public:
         return node;
     }
 
-    TreeNode* create(vector<int>& inorder, int in_l, int in_r,
-                     vector<int>& postorder, int post_l, int post_r) {
-        if (post_l > post_r) {
+    TreeNode* create(vector<int>& inorder, int in_start, int in_end,
+                     vector<int>& postorder, int post_start, int post_end) {
+        if (post_start > post_end) {
             return nullptr;
         }
-        int root_val = postorder.at(post_r);
+        int root_val = postorder[post_end];
         TreeNode* root = new TreeNode(root_val);
-        int k = 0;
-        for (k = in_l; k <= in_r; ++k) {
-            if (inorder.at(k) == root_val) {
+        int inorder_root_idx = 0;
+        for (inorder_root_idx = in_start; inorder_root_idx <= in_end; ++inorder_root_idx) {
+            if (inorder[inorder_root_idx] == root_val) {
                 break;
             }
         }
-        int num_left = k - in_l;
-        root->left = create(inorder, in_l, in_l + num_left - 1,
-                            postorder, post_l, post_l + num_left - 1);
-        root->right = create(inorder, k + 1, in_r,
-                             postorder, post_l + num_left, post_r - 1);
+        int num_left = inorder_root_idx - in_start;
+        root->left = create(inorder, in_start, inorder_root_idx - 1,
+                            postorder, post_start, post_start + num_left - 1);
+        root->right = create(inorder, inorder_root_idx + 1, in_end,
+                             postorder, post_start + num_left, post_end - 1);
         return root;
     }
 };
