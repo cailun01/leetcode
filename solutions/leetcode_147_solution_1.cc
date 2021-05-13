4372a5dea11c551a2c149c9a95f1171d5bc1207c
cailun01 <1354213521@qq.com>
2021-05-13 11:21:36 +0800

    change notes and codes to leetcode 147

--- solutions/leetcode_147_solution_1.cc
+++ solutions/leetcode_147_solution_1.cc
@@ -1,32 +1,32 @@
 #include "headers.h"
-// 插入排序
-// 对序列A的n个元素A[1] ~ A[n]，令i从2到n枚举，进行n-1趟操作。
-// 假设某一趟时，序列A的前i-1个元素A[1]~A[i-1]已经有序，而范围[i, n]还未有序，
-// 那么该趟从范围[1, i-1]中寻找某个位置j，将A[i]插入到位置j，范围A[1, i]有序。
+// 数组的插入排序，leetcode_912_solution_5.cc
+// 本题解释：https://leetcode-cn.com/problems/insertion-sort-list/solution/dui-lian-biao-jin-xing-cha-ru-pai-xu-by-leetcode-s/
+
 class Solution {
  public:
 	ListNode* insertionSortList(ListNode* head) {
-		if (!head || !head->next)
+		if (!head || !head->next) {
 			return head;
-		ListNode *dummyhead = new ListNode(-1);//伪头指针
-		dummyhead->next = head;
-		ListNode *prev = head;
-		ListNode *node = head->next; // node节点将被插入到前面的某个位置, 在node之前，链表是有序的
-		while (node) {
-			if (node->val < prev->val) { // 如果后一节点的值小于前一节点
-				ListNode* temp = dummyhead;//！！temp要等于dummyhead，这样才可以比较第一个元素
-				while (temp->next->val < node->val) {//！！！这里是temp->next：因为要修改前面的temp的指向
-					temp = temp->next;//指针后移
-				}
-				prev->next = node->next;
-				node->next = temp->next;
-				temp->next = node;
-				node = prev->next;//此时不用改变prev指向！因为prev没有变，只是待排序元素变了位置。
-			} else { // 如果后一节点的值大于等于前一节点
-				prev = prev->next;
-				node = node->next;
-			}
 		}
-		return dummyhead->next;//!!!不能返回head！！因为后面会改变head所指向内存的位置！
+		ListNode *dummy = new ListNode(-1);//伪头指针
+		dummy->next = head;
+		ListNode *last_sorted = head; // last_sorted为链表已经排好序的最后一个节点并初始化
+		ListNode *curr = head->next; // node节点为待插入的元素, 在node之前，链表是有序的
+		while (curr) {
+			if (last_sorted->val <= curr->val) {// 说明curr应该位于lastSorted之后
+				last_sorted = last_sorted->next;  // 将lastSorted后移一位,curr变成新的lastSorted
+			} else {                            // 否则,从链表头结点开始向后遍历链表中的节点
+				ListNode* prev = dummy;           // 从链表头开始遍历 prev是插入节点curr位置的前一个节点
+				while (prev->next->val <= curr->val) {  // 循环退出的条件是找到curr应该插入的位置
+					prev = prev->next;
+				}
+				// 以下三行是为了完成对curr的插入
+				last_sorted->next = curr->next;
+				curr->next = prev->next;
+				prev->next = curr;
+			}
+			curr = last_sorted->next; // 此时 curr 为下一个待插入的元素
+		}
+		return dummy->next;
 	}
 };
