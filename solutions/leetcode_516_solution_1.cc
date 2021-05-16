4af5a5cb2f0205b8c93b778bdaa9db15c48db0dd
cailun01 <1354213521@qq.com>
2021-05-16 10:14:13 +0800

    add notes to dynamic programming problems

--- solutions/leetcode_516_solution_1.cc
+++ solutions/leetcode_516_solution_1.cc
@@ -1,8 +1,30 @@
+/*
+给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+
+示例 1:
+输入:
+"bbbab"
+输出:
+4
+一个可能的最长回文子序列为 "bbbb"。
+
+示例 2:
+输入:
+"cbbd"
+输出:
+2
+一个可能的最长回文子序列为 "bb"。
+*/
+
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
         // 在子数组array[i..j]中，我们要求的子序列（最长回文子序列）的长度为dp[i][j]。
         int n = s.size();
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        
         // dp 数组全部初始化为 0
         vector<vector<int>> dp(n, vector<int>(n, 0));
         // base case
@@ -10,7 +32,7 @@ public:
             dp[i][i] = 1;
         }
         // 反着遍历保证正确的状态转移
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {
             for (int j = i + 1; j < n; j++) {
                 // 状态转移方程
                 if (s[i] == s[j])
