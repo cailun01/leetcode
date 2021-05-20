18dc7a8922bd39f03bbfc55b8aad20c69dc0bad5
cailun01 <1354213521@qq.com>
2021-05-20 23:48:50 +0800

    add notes

--- solutions/leetcode_297_solution_1.cc
+++ solutions/leetcode_297_solution_1.cc
@@ -50,27 +50,16 @@ public:
         return str;
     }
 
-
-    vector<string> split(const string& str, const string& delim) {  
-      vector<string> res;  
-      if("" == str) return res;  
-      //先将要切割的字符串从string类型转换为char*类型  
-      char * strs = new char[str.length() + 1] ; //不要忘了  
-      strcpy(strs, str.c_str());   
-    
-      char * d = new char[delim.length() + 1];  
-      strcpy(d, delim.c_str());  
-    
-      char *p = strtok(strs, d);  
-      while(p) {  
-        string s = p; //分割得到的字符串转换为string类型  
-        res.push_back(s); //存入结果数组  
-        p = strtok(NULL, d);  
-      }  
-    
-      return res;  
+    std::vector<std::string> split(const std::string& s, const char& delimiter) {
+        std::vector<std::string> tokens;
+        std::string token;
+        std::istringstream tokenStream(s);
+        while (std::getline(tokenStream, token, delimiter)) {
+            tokens.emplace_back(token);
+        }
+        return tokens;
     }
-    
+
     TreeNode* deserialize(list<string>& nodes) {
         if (nodes.empty()) {
             return nullptr;
@@ -90,7 +79,7 @@ public:
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         list<string> nodes;
-        for (auto s : split(data, ",")) {
+        for (auto s : split(data, ',')) {
             nodes.push_back(s);
         }
         return deserialize(nodes);
--- solutions/leetcode_474_solution_1.cc
+++ solutions/leetcode_474_solution_1.cc
@@ -1,37 +1,57 @@
+/*
+题目：
+给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+请你找出并返回 strs 的最大子集的大小，该子集中最多有 m 个 0 和 n 个 1 。
+如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+
+
+示例 1：
+输入：strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
+输出：4
+解释：最多有 5 个 0 和 3 个 1 的最大子集是 {"10","0001","1","0"} ，因此答案是 4 。
+其他满足题意但较小的子集包括 {"0001","1"} 和 {"10","1","0"} 。
+{"111001"} 不满足题意，因为它含 4 个 1 ，大于 n 的值 3 。
+
+示例 2：
+输入：strs = ["10", "0", "1"], m = 1, n = 1
+输出：2
+解释：最大的子集是 {"0", "1"} ，所以答案是 2 
+*/
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        int num_strs = strs.size();
-        // dp 取前i个字符串时，有j个0和k个1时，最大子集的大小
-        vector<vector<vector<int>>> dp(num_strs + 1, 
-            vector<vector<int>>(m + 1, vector<int>(n + 1, 0)));
-        for (int i = 1; i <= num_strs; ++i) {
-            auto num_zeros_and_ones = count_zeros_and_ones(strs[i - 1]);
-            int num_zeros = std::get<0>(num_zeros_and_ones);
-            int num_ones = std::get<1>(num_zeros_and_ones);
-            for (int j = 0; j <= m; ++j) {
-                for (int k = 0; k <= n; ++k) {
-                    if (num_zeros > j || num_ones > k) {
-                        dp[i][j][k] = dp[i - 1][j][k];
-                    } else {
-                        dp[i][j][k] = max(dp[i - 1][j][k],
-                            dp[i - 1][j - num_zeros][k - num_ones] + 1);
-                    }
-                }
-            }
+  int findMaxForm(vector<string>& strs, int m, int n) {
+    int num_strs = strs.size();
+    // dp 取前i个字符串时，有j个0和k个1时，最大子集的大小
+    vector<vector<vector<int>>> dp(num_strs + 1, 
+        vector<vector<int>>(m + 1, vector<int>(n + 1, 0)));
+    for (int i = 1; i <= num_strs; ++i) {
+      auto num_zeros_and_ones = count_zeros_and_ones(strs[i - 1]);
+      int num_zeros = std::get<0>(num_zeros_and_ones);
+      int num_ones = std::get<1>(num_zeros_and_ones);
+      for (int j = 0; j <= m; ++j) {
+        for (int k = 0; k <= n; ++k) {
+          // 当前容量装不下
+          if (num_zeros > j || num_ones > k) {
+            dp[i][j][k] = dp[i - 1][j][k];
+        } else {
+            dp[i][j][k] = max(dp[i - 1][j][k],
+                dp[i - 1][j - num_zeros][k - num_ones] + 1);
+          }
         }
-        return dp[num_strs][m][n];
+      }
     }
+    return dp[num_strs][m][n];
+  }
 
-    tuple<int, int> count_zeros_and_ones(string str) {
-        int num_zeros = 0, num_ones = 0;
-        for (const auto& c : str) {
-            if (c == '0') {
-                num_zeros++;
-            } else if (c == '1') {
-                num_ones++;
-            }
-        }
-        return std::make_tuple(num_zeros, num_ones);
+  tuple<int, int> count_zeros_and_ones(string str) {
+    int num_zeros = 0, num_ones = 0;
+    for (const auto& c : str) {
+      if (c == '0') {
+        num_zeros++;
+      } else if (c == '1') {
+        num_ones++;
+      }
     }
+    return std::make_tuple(num_zeros, num_ones);
+  }
 };
