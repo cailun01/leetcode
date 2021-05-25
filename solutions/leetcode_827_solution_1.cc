dcf7b948cbb5d49e2a816aa0424e29b78e8a3bd4
cailun01 <1354213521@qq.com>
2021-05-25 23:49:31 +0800

    add notes to lc 827

--- solutions/leetcode_827_solution_1.cc
+++ solutions/leetcode_827_solution_1.cc
@@ -1,11 +1,9 @@
 class Solution {
 public:
   int row, col;
-  int idx = 2;
+  int idx = 2; // idx作为每个岛屿的记号。因为0是海洋，1是陆地。所以idx从2开始。
   int dx[4] = {0, 1, 0, -1};
   int dy[4] = {1, 0, -1, 0};
-  //记录每个岛屿的面积，a中的index与岛屿标记的g[i][j]的值idx对应相等
-  int a[20000];
 
   int dfs(int x, int y, vector<vector<int>>& grid){
     if(!inArea(x, y) || grid[x][y] != 1) {
@@ -28,27 +26,31 @@ public:
     if(row <= 0) return 0;
     col = grid[0].size();
     int res = 0;
+    unordered_map<int, int> island_area; // key: idx, value: area of island
     for(int i = 0; i < row; i++){
       for(int j = 0; j < col; j++){
         if(grid[i][j] == 1){
-          a[idx] = dfs(i, j, grid);
-          res = max(res, a[idx]);
+          island_area[idx] = dfs(i, j, grid);
+          res = max(res, island_area[idx]);
           idx++;
         }
       }
     }
-    
+    if (res == 0) {
+      //如果没有陆地，那么就造1块
+      return 1;
+    } 
     for(int i = 0; i < row; i++){
       for(int j = 0; j < col; j++){
-        if(grid[i][j] == 0){
+        if(grid[i][j] == 0) { //遍历海洋格子
           int count = 1;
-          unordered_map<int, int> q;
-          for(int idx = 0; idx < 4; idx++){
-            int x = i + dx[idx];
-            int y = j + dy[idx];
-            if(inArea(x, y) && grid[x][y] >= 2 && q.find(grid[x][y]) == q.end()){
-              count += a[grid[x][y]];
-              q[grid[x][y]] = 0;
+          unordered_set<int> q; //把上下左右四邻放入set里，set的目的是去重
+          for(int k = 0; k < 4; k++){
+            int x = i + dx[k];
+            int y = j + dy[k];
+            if(inArea(x, y) && grid[x][y] >= 2 && q.find(grid[x][y]) == q.end()){ // 如果grid[x][y]是岛屿
+              count += island_area[grid[x][y]];
+              q.insert(grid[x][y]);
             } 
           }
           res = max(res, count);   
