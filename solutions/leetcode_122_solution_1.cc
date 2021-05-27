1570f56d418ee487e355d32af0a335db45c5d036
cailun01 <1354213521@qq.com>
2021-05-27 23:18:39 +0800

    lc 123

--- solutions/leetcode_122_solution_1.cc
+++ solutions/leetcode_122_solution_1.cc
@@ -1,38 +1,36 @@
-#include "headers.h"
-// 提交超时
+/*122. 买卖股票的最佳时机 II
+给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
 
+示例 1:
+输入: prices = [7,1,5,3,6,4]
+输出: 7
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
 
-// 每天都有三种动作：买入（buy）、卖出（sell）、无操作（rest）
-// 因为不限制交易次数，因此交易次数这个因素不影响题目，不必考虑。
-// DP Table 是二维的，两个维度分别是天数（0,1,...,n-1）和是否持有股票（1 表持有，0 表不持有）。
+示例 2:
+输入: prices = [1,2,3,4,5]
+输出: 4
+解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。因为这样属于同时参与了多笔交易，
+     你必须在再次购买前出售掉之前的股票。
 
-// Case 1，今天我没有股票，有两种可能：
-//   昨天我手上就没有股票，今天不做任何操作（rest）；
-//   昨天我手上有一只股票，今天按照时价卖掉了（sell），收获了一笔钱
-
-// Case 2，今天持有一只股票，有两种可能：
-//   昨天我手上就有这只股票，今天不做任何操作（rest）；
-//   昨天我没有股票，今天按照时价买入一只（sell），花掉了一笔钱
-// 综上，第 i 天的状态转移方程为：
-// dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
-// dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
-
+示例 3:
+输入: prices = [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+*/
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int num_days = prices.size();
-        if (num_days <= 1) return 0;
-        // dp表示手中有多少钱
-        vector<vector<int>> dp(num_days, vector<int>(num_days, -1));
-        dp[0][0] = 0; //  第一天没有股票，说明没买没卖，获利为0
-        dp[0][1] = -prices[0];   // 第一天持有股票，说明买入了，花掉一笔钱
-        for (int i = 1; i < num_days; ++i) {
-          dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
-          dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
-        }
-        return dp[num_days - 1][0];    // 返回最后一天且手上没有股票时的获利情况
+  int maxProfit(vector<int>& prices) {
+    int dp0 = 0;            // 手里没股票
+    int dp1 = - prices[0];  // 手里有股票
+    for(int i = 1; i < prices.size(); i++){
+      dp0 = max(dp0, dp1 + prices[i]);
+      dp1 = max(dp1, dp0 - prices[i]);
     }
+    return dp0;
+  }
 };
-
-// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/solution/gu-piao-jiao-yi-xi-lie-tan-xin-si-xiang-he-dong-2/
