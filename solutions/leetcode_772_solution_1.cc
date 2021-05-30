e021ffc6b635d47b6c5ad5da9dc87825c40c1306
cailun01 <1354213521@qq.com>
2021-05-30 23:08:33 +0800

    add notes to lc 772

--- solutions/leetcode_772_solution_1.cc
+++ solutions/leetcode_772_solution_1.cc
@@ -36,61 +36,61 @@ s 是一个 有效的 表达式
 
 class Solution {
 public:
-    // 定义一个成员变量i，在递归时就可以处于正确的位置。
-    int i = 0;
-    int calculate(string s) {
-        if (s.size() == 0) {
-            return 0;
-        }
-
-        int num = 0;
-        char sign = '+';
-        stack<int> stk;
-        
-        while (i < s.size()) {
-            char c = s[i++];
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0');
-            }
-
-            if (c == '(') {
-                num = calculate(s);
-            }
-
-            if (!isdigit(c) && c != ' ' || i == s.size()) {
-                int pre;
-                switch (sign) {
-                    case '+':
-                        stk.push(num);
-                        break;
-                    case '-':
-                        stk.push(-num);
-                        break;
-                    case '*':
-                        pre = stk.top();
-                        stk.pop();
-                        stk.push(pre * num);
-                        break;
-                    case '/':
-                        pre = stk.top();
-                        stk.pop();
-                        stk.push(pre / num);
-                        break;
-                }
-                sign = c;
-                num = 0;
-            }
-
-            if (c == ')') {
-                break;
-            }
-        }
-
-        int res = 0;
-        while (!stk.empty()) {
-            res += stk.top();
-            stk.pop();
-        }
-        return res;
+  // 定义一个成员变量i，在递归时就可以处于正确的位置。
+  int i = 0;
+  int calculate(string s) {
+    if (s.size() == 0) {
+      return 0;
     }
+
+    int num = 0;
+    char sign = '+';
+    stack<int> stk;
+    
+    while (i < s.size()) {
+      char c = s[i++];
+      if (isdigit(c)) {
+        num = num * 10 + (c - '0');
+      }
+
+      if (c == '(') {
+        num = calculate(s);
+      }
+
+      if (!isdigit(c) && c != ' ' || i == s.size()) {
+        int pre;
+        switch (sign) {
+          case '+':
+            stk.push(num);
+            break;
+          case '-':
+            stk.push(-num);
+            break;
+          case '*':
+            pre = stk.top();
+            stk.pop();
+            stk.push(pre * num);
+            break;
+          case '/':
+            pre = stk.top();
+            stk.pop();
+            stk.push(pre / num);
+            break;
+        }
+        sign = c;
+        num = 0;
+      }
+
+      if (c == ')') {
+        break;
+      }
+    }
+
+    int res = 0;
+    while (!stk.empty()) {
+      res += stk.top();
+      stk.pop();
+    }
+    return res;
+  }
 };
