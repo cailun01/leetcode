d3372e38cc3a5423b49ad200ddf876fd19fb73c6
cailun01 <1354213521@qq.com>
2021-06-01 21:44:48 +0800

    add notes to 247

--- solutions/leetcode_147_solution_1.cc
+++ solutions/leetcode_147_solution_1.cc
@@ -3,21 +3,28 @@
 // 本题解释：https://leetcode-cn.com/problems/insertion-sort-list/solution/dui-lian-biao-jin-xing-cha-ru-pai-xu-by-leetcode-s/
 
 class Solution {
- public:
+public:
 	ListNode* insertionSortList(ListNode* head) {
 		if (!head || !head->next) {
 			return head;
 		}
 		ListNode *dummy = new ListNode(-1);//伪头指针
 		dummy->next = head;
-		ListNode *last_sorted = head; // last_sorted为链表已经排好序的最后一个节点并初始化
-		ListNode *curr = head->next; // node节点为待插入的元素, 在node之前，链表是有序的
+		// last_sorted为链表已经排好序的最后一个节点并初始化
+		ListNode *last_sorted = head;
+		// node节点为待插入的元素, 在node之前，链表是有序的
+		ListNode *curr = head->next; 
 		while (curr) {
-			if (last_sorted->val <= curr->val) {// 说明curr应该位于lastSorted之后
-				last_sorted = last_sorted->next;  // 将lastSorted后移一位,curr变成新的lastSorted
-			} else {                            // 否则,从链表头结点开始向后遍历链表中的节点
-				ListNode* prev = dummy;           // 从链表头开始遍历 prev是插入节点curr位置的前一个节点
-				while (prev->next->val <= curr->val) {  // 循环退出的条件是找到curr应该插入的位置
+			if (last_sorted->val <= curr->val) {
+				// 说明curr应该位于lastSorted之后
+				// 将lastSorted后移一位,curr变成新的lastSorted
+				last_sorted = last_sorted->next;  
+			} else {
+				// 否则,从链表头结点开始向后遍历链表中的节点
+				// 从链表头开始遍历 prev是插入节点curr位置的前一个节点        
+				ListNode* prev = dummy;           
+				while (prev->next->val <= curr->val) {
+					// 循环退出的条件是找到curr应该插入的位置
 					prev = prev->next;
 				}
 				// 以下三行是为了完成对curr的插入
--- solutions/leetcode_148_solution_1.cc
+++ solutions/leetcode_148_solution_1.cc
@@ -1,4 +1,4 @@
-/**
+/** 排序链表
 归并排序链表
 用快慢指针把链表分为两部分
 左边链表排序
@@ -7,52 +7,53 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* left, ListNode* right) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* cur = dummy;
-        while (left && right) {
-            if (left->val <= right->val) {
-                cur->next = left;
-                cur = cur->next;
-                left = left->next;
-            } else {
-                cur->next = right;
-                cur = cur->next;
-                right = right->next;
-            }
-        }
-        if (left) {
-            cur->next = left;
-        }
-        if (right) {
-            cur->next = right;
-        }
-        return dummy->next;
+  ListNode* merge(ListNode* left, ListNode* right) {
+    ListNode* dummy = new ListNode(-1);
+    ListNode* cur = dummy;
+    while (left && right) {
+      if (left->val <= right->val) {
+        cur->next = left;
+        cur = cur->next;
+        left = left->next;
+      } else {
+        cur->next = right;
+        cur = cur->next;
+        right = right->next;
+      }
+    }
+    if (left) {
+      cur->next = left;
+    }
+    if (right) {
+      cur->next = right;
+    }
+    return dummy->next;
+  }
+
+  ListNode* sortList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
     }
 
-    ListNode* sortList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) {
-            return head;
-        }
-
-        // 快慢指针，找链表中点
-        // 如果有2个中点(链表长度为偶数)，则找到靠左的中点
-        ListNode* fast = head;
-        ListNode* slow = head;
-        while (fast->next && fast->next->next) {
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        // 拆分链表
-        ListNode* tail = slow;
-        slow = slow->next;
-        tail->next = nullptr;
-        
-        // 左边排序后返回左链表的开头
-        ListNode* left = sortList(head);
-        // 右边排序后返回右链表的开头
-        ListNode* right = sortList(slow);
-        // 合并
-        return merge(left, right);
+    // 快慢指针，找链表中点
+    // 如果有2个中点(链表长度为偶数)，则找到靠左的中点
+    ListNode* fast = head;
+    ListNode* slow = head;
+    while (fast->next && fast->next->next) {
+      fast = fast->next->next;
+      slow = slow->next;
     }
+    // 拆分链表，tail是左侧链表的最后一个节点
+    // 它的作用是tail->next = nullptr;
+    ListNode* tail = slow;
+    slow = slow->next;
+    tail->next = nullptr;
+    
+    // 左边排序后返回左链表的开头
+    ListNode* left = sortList(head);
+    // 右边排序后返回右链表的开头
+    ListNode* right = sortList(slow);
+    // 合并
+    return merge(left, right);
+  }
 };
