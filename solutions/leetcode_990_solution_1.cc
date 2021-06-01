5756e6cafed4b35cf2becaa1b71cff5fba1b2f2b
cailun01 <1354213521@qq.com>
2021-06-01 20:11:04 +0800

    add notes

--- solutions/leetcode_990_solution_1.cc
+++ solutions/leetcode_990_solution_1.cc
@@ -1,5 +1,5 @@
 #include "headers.h"
-/*
+/* 990 等式方程的可满足性
 给定一个由表示变量之间关系的字符串方程组成的数组，每个字符串方程 equations[i] 的长度为4，
 并采用两种不同的形式之一："a==b" 或 "a!=b"。在这里，a 和 b 是小写字母（不一定不同），表示单字母变量名。
 只有当可以将整数分配给变量名，以便满足所有给定的方程时才返回 true，否则返回 false。 
@@ -44,7 +44,7 @@ public:
       }
     }
     // 检查不等关系是否打破相等关系的连通性
-    for (auto eq:equations) {
+    for (auto eq : equations) {
       if (eq[1] == '!') {
         int x = find(eq[0] - 'a', parent);
         int y = find(eq[3] - 'a', parent);
