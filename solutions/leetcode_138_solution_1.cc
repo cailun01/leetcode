d7bac920699ed2f29b76d1f8971448c1a33f53b8
cailun01 <1354213521@qq.com>
2021-06-02 08:35:26 +0800

    change tabs

--- solutions/leetcode_138_solution_1.cc
+++ solutions/leetcode_138_solution_1.cc
@@ -5,36 +5,36 @@ class Solution {
 private:
     unordered_map<Node*, Node*> visited;
 public:
-    Node* copyRandomList(Node* head) {
-        if (!head) return head;
+  Node* copyRandomList(Node* head) {
+    if (!head) return head;
 
-        Node* clone_node = new Node(head->val);
-        visited[head] = clone_node;
+    Node* clone_node = new Node(head->val);
+    visited[head] = clone_node;
 
-        queue<Node*> q;
-        q.push(head);
+    queue<Node*> q;
+    q.push(head);
 
-        while (!q.empty()) {
-            Node* cur = q.front();
-            q.pop();
-            
-            if (cur->next) {
-                if (visited.find(cur->next) == visited.end()) {
-                    Node* clone_cur_next = new Node(cur->next->val);
-                    visited[cur->next] = clone_cur_next;
-                    q.push(cur->next);
-                }
-                visited[cur]->next = visited[cur->next];
-            }
-            if (cur->random) {
-                if (visited.find(cur->random) == visited.end()) {
-                    Node* clone_cur_random = new Node(cur->random->val);
-                    visited[cur->random] = clone_cur_random;
-                    q.push(cur->random);
-                }
-                visited[cur]->random = visited[cur->random];
-            }
+    while (!q.empty()) {
+      Node* cur = q.front();
+      q.pop();
+      
+      if (cur->next) {
+        if (visited.find(cur->next) == visited.end()) {
+          Node* clone_cur_next = new Node(cur->next->val);
+          visited[cur->next] = clone_cur_next;
+          q.push(cur->next);
         }
-        return clone_node;
+        visited[cur]->next = visited[cur->next];
+      }
+      if (cur->random) {
+        if (visited.find(cur->random) == visited.end()) {
+          Node* clone_cur_random = new Node(cur->random->val);
+          visited[cur->random] = clone_cur_random;
+          q.push(cur->random);
+        }
+        visited[cur]->random = visited[cur->random];
+      }
     }
+    return clone_node;
+  }
 };
--- solutions/leetcode_279_solution_2.cc
+++ solutions/leetcode_279_solution_2.cc
@@ -2,28 +2,28 @@
 // BFS，性能较差
 class Solution {
 public:
-    int numSquares(int n) {
-        set<int> visited;
-        queue<pair<int, int>> q; // pair的first是节点，second是节点n到节点node的步数
-        q.push({n, 0});
-        while (!q.empty()) {
-            int num = q.front().first;
-            int step = q.front().second;
-            q.pop();
-            vector<int> graph;
-            for (int i = 1; i <= (int)sqrt(n); ++i) {
-                graph.push_back(num - i * i);
-            }
-            for (int & node : graph) {
-                if (node == 0) {
-                    return step + 1;
-                }
-                if (visited.find(node) == visited.end()) {
-                    visited.insert(node);
-                    q.push({node, step + 1});
-                }
-            }
+  int numSquares(int n) {
+    set<int> visited;
+    queue<pair<int, int>> q; // pair的first是节点，second是节点n到节点node的步数
+    q.push({n, 0});
+    while (!q.empty()) {
+      int num = q.front().first;
+      int step = q.front().second;
+      q.pop();
+      vector<int> graph;
+      for (int i = 1; i <= (int)sqrt(n); ++i) {
+        graph.push_back(num - i * i);
+      }
+      for (int & node : graph) {
+        if (node == 0) {
+          return step + 1;
         }
-        return 0;
+        if (visited.find(node) == visited.end()) {
+          visited.insert(node);
+          q.push({node, step + 1});
+        }
+      }
     }
+    return 0;
+  }
 };
