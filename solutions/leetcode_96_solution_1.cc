d24d8b8bdfbee29906c71cee527aa01f25478995
cailun01 <1354213521@qq.com>
2021-06-06 17:26:33 +0800

    lc 96

---
+++ solutions/leetcode_96_solution_1.cc
@@ -0,0 +1,39 @@
+/* 96. 不同的二叉搜索树
+给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+
+示例 1：
+输入：n = 3
+输出：5
+
+示例 2：
+输入：n = 1
+输出：1
+*/
+
+/* 左子树大于根节点，右子树小于根节点。
+二叉搜索树的中序遍历是一个有序列表。 
+*/
+
+class Solution {
+public:
+  int numTrees(int n) {
+    // 用连续的 i 个数，所构建出的 BST 种类数
+    vector<int> dp(n + 1); 
+    dp[0] = dp[1] = 1;//初始化
+    dfs(n, dp);
+    return dp[n]; 
+  }
+    
+  int dfs(int n, vector<int>& dp){
+    //如果已经算过的，直接返回 
+    if (dp[n] != 0) { 
+      return dp[n];
+    }
+    int ans = 0;
+	  for (int i = 1; i <= n; i++) {
+      ans += dfs(i-1, dp) * dfs(n-i, dp);//递归
+	  }
+    dp[n] = ans;//记忆
+    return ans;
+	} 
+};
