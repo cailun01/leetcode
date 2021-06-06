e9f7ab4f2dcd9503fefa8a6b7307d5fb7754775c
cailun01 <1354213521@qq.com>
2021-06-06 16:23:11 +0800

    leetcode 97

---
+++ solutions/leetcode_97_solution_1.cc
@@ -0,0 +1,56 @@
+/* 97. 交错字符串
+给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+
+两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+提示：a + b 意味着字符串 a 和 b 连接。
+
+示例 1：
+输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+输出：true
+
+示例 2：
+输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+输出：false
+
+示例 3：
+输入：s1 = "", s2 = "", s3 = ""
+输出：true
+*/
+
+class Solution {
+  bool DFS(string &s1, string &s2, string &s3, int i, int j, int k, vector<vector<int>>& memory) {
+    if (memory[i][j] != -1) { 
+      return memory[i][j] == 1;
+    }
+    if (k == s3.size()) {
+      memory[i][j] = 1;
+      return true;
+    }
+    bool res = false;
+    if (i < s1.size() && s1[i] == s3[k]) {
+      res = DFS(s1, s2, s3, i+1, j, k+1, memory);
+    }
+    if (j < s2.size() && s2[j] == s3[k]) {
+      res = res || DFS(s1, s2, s3, i, j+1, k+1, memory);
+    }
+    if (res){
+      memory[i][j] = 1;
+      } else {
+      memory[i][j] = 0;
+    }
+    return res;
+  }
+public:
+  bool isInterleave(string s1, string s2, string s3) {
+    if (s1.size() + s2.size() != s3.size()) {
+      return false;
+    }
+    vector<vector<int>> memory(s1.size() + 1, vector<int>(s2.size() + 1, -1));
+    return DFS(s1, s2, s3, 0, 0, 0, memory);
+  }
+};
