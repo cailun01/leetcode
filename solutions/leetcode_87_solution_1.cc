574c0945519190015e6f4a0f614019cedd705499
cailun01 <1354213521@qq.com>
2021-06-09 15:33:25 +0800

    add notes to lc 87

--- solutions/leetcode_87_solution_1.cc
+++ solutions/leetcode_87_solution_1.cc
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isScramble(string s1, string s2) {
-    	if(s1 == s2) return true;
-    	if(s1.size() != s2.size()) return false;
+    	if(s1 == s2) { 
+        return true; 
+      }
+    	if(s1.size() != s2.size()) { 
+        return false; 
+      }
 
     	int n = s1.size();
       // dp[i][j][len] 代表s1从i开始，s2从j开始，后面长度为len的字符是否能形成「扰乱字符串」（互为翻转）。
@@ -16,15 +20,18 @@ public:
           }
     		}
     	}
+      // 假设划分后，s1的两个子串是s1_sub1和s1_sub2。s2的两个子串为s2_sub1和s2_sub2
 
     	// 再处理其余长度情况
     	for (int len = 2; len <= n; len++) {
     		for (int i = 0; i + len <= n; i++) {
     			for (int j = 0; j + len <= n; j++) {
+            // 枚举划分位置k
     				for (int k = 1; k < len; k++) {
-              // 对应了「s1 的 [0, k) & [k, n)」匹配「s2 的 [0, k) & [k, n)」
+              // s1_sub1匹配s2_sub1, s1_sub2匹配s2_sub2。
     					bool a = dp[i][j][k] && dp[i + k][j + k][len - k];
-              // 对应了「s1 的 [0,k) & [i,n)」匹配「s2 的 [n-k, n) & [0,n - k)」
+              // s1_sub1匹配s2_sub2, s1_sub2匹配s2_sub1。
+              // s1_sub1起点i，s2_sub2起点 j + 前面那段长度len-k，s1_sub2起点i+前面长度k
     					bool b = dp[i][j + len - k][k] && dp[i + k][j][len - k];
     					if(a || b) {
     						dp[i][j][len] = true;
