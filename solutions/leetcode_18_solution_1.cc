86683fb957ca4f7f22da814850384d2d753e8163
cailun01 <1354213521@qq.com>
2021-06-11 21:55:42 +0800

    add notes to lc

--- solutions/leetcode_18_solution_1.cc
+++ solutions/leetcode_18_solution_1.cc
@@ -9,44 +9,44 @@
 
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if (nums.size() < 4) return {};
-        int num_elems = nums.size();
-        sort(nums.begin(),nums.end());
-        int a, b, c, d;
-        vector<vector<int>> res;
-        for (a = 0; a < num_elems - 3; ++a) {
-            if (a > 0 && nums[a] == nums[a - 1]) {
-                continue;
-            }
-            for (int b = a + 1; b < num_elems - 2; ++b) {
-                if (b > a + 1 && nums[b] == nums[b - 1]) {
-                    continue;
-                }
-                c = b + 1;
-                d = num_elems - 1;
-                while (c < d) {
-                    int sum = nums[a] + nums[b] + nums[c] + nums[d];
-                    if (sum == target) {
-                        res.push_back({nums[a],nums[b],nums[c],nums[d]});
-                        while (c < d && nums[c] == nums[c + 1]) {
-                            ++c;
-                        }
-                        ++c;
-                        while (c < d && nums[d] == nums[d - 1]) {
-                            --d;
-                        }
-                        --d;
-                    } else if (sum < target) {
-                        ++c;
-                    } else {
-                        --d;
-                    }
-                }
-                
-            }
-            
+  vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    if (nums.size() < 4) return {};
+    int num_elems = nums.size();
+    sort(nums.begin(), nums.end());
+    int a, b, c, d;
+    vector<vector<int>> res;
+    for (a = 0; a < num_elems - 3; ++a) {
+      if (a > 0 && nums[a] == nums[a - 1]) {
+        // 去重
+        continue;
+      }
+      for (int b = a + 1; b < num_elems - 2; ++b) {
+        if (b > a + 1 && nums[b] == nums[b - 1]) {
+          // 去重
+          continue;
         }
-        return res;
+        c = b + 1;
+        d = num_elems - 1;
+        while (c < d) {
+          int sum = nums[a] + nums[b] + nums[c] + nums[d];
+          if (sum == target) {
+            res.push_back({nums[a], nums[b], nums[c], nums[d]});
+            while (c < d && nums[c] == nums[c + 1]) {
+              ++c;
+            }
+            ++c;
+            while (c < d && nums[d] == nums[d - 1]) {
+              --d;
+            }
+            --d;
+          } else if (sum < target) {
+            ++c;
+          } else {
+            --d;
+          }
+        } 
+      }
     }
+    return res;
+  }
 };
--- solutions/leetcode_19_solution_1.cc
+++ solutions/leetcode_19_solution_1.cc
@@ -1,49 +1,53 @@
 #include "headers.h"
-// 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+/* 19. 删除链表的倒数第 N 个结点
+给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
 
-// 示例：
+示例：
 
-// 给定一个链表: 1->2->3->4->5, 和 n = 2.
+给定一个链表: 1->2->3->4->5, 和 n = 2.
 
-// 当删除了倒数第二个节点后，链表变为 1->2->3->5.
-// 说明：
+当删除了倒数第二个节点后，链表变为 1->2->3->5.
+说明：
 
-// 给定的 n 保证是有效的。
+给定的 n 保证是有效的。
 
-// 进阶：
-// 你能尝试使用一趟扫描实现吗？
+进阶：
+你能尝试使用一趟扫描实现吗？
+*/
 
-// 题解：
-// 假设链表长度为L，倒数第n个节点相当于正数第L-n+1个节点
-// 删除倒数第n个节点的过程是把倒数第n+1个节点的next直接设置为倒数第n-1个节点
-// 也就是把正数第L-n个节点next直接设置为正数L-n+2个节点
+/*
+题解：
+假设链表长度为L，倒数第n个节点相当于正数第L-n+1个节点
+删除倒数第n个节点的过程是把倒数第n+1个节点的next直接设置为倒数第n-1个节点
+也就是把正数第L-n个节点next直接设置为正数L-n+2个节点
 
-// 首先我们将添加一个哑结点作为辅助，该结点位于列表头部。
-// 哑结点用来简化某些极端情况，例如列表中只含有一个结点，或需要删除列表的头部。
-// 在第一次遍历中，我们找出列表的长度L。然后设置一个指向哑结点的指针，
-// 并移动它遍历列表，直至它到达第 (L - n)个结点那里。
-// 我们把第(L - n)个结点的 next 指针重新链接至第(L - n + 2)个结点，完成这个算法。
+首先我们将添加一个哑结点作为辅助，该结点位于列表头部。
+哑结点用来简化某些极端情况，例如列表中只含有一个结点，或需要删除列表的头部。
+在第一次遍历中，我们找出列表的长度L。然后设置一个指向哑结点的指针，
+并移动它遍历列表，直至它到达第 (L - n)个结点那里。
+我们把第(L - n)个结点的 next 指针重新链接至第(L - n + 2)个结点，完成这个算法。
+*/
 
 // 扫描两趟
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-      ListNode* dummy = new ListNode(-1);
-      dummy->next = head;
-      ListNode* first = head;
-      int length = 1;
-      while (head->next) {
-        length += 1;
-        head = head->next;
-      }
-      length -= n; // L - n
-      first = dummy;
-      while (length--) {
-        first = first->next;
-      }
-      first->next = first->next->next;
-      return dummy->next;
+  ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* dummy = new ListNode(-1);
+    dummy->next = head;
+    ListNode* first = head;
+    int length = 1;
+    while (head->next) {
+      length += 1;
+      head = head->next;
     }
+    length -= n; // L - n
+    first = dummy;
+    while (length--) {
+      first = first->next;
+    }
+    first->next = first->next->next;
+    return dummy->next;
+  }
 };
 
 // 确定链表的长度
