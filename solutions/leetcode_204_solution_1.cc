5ecc19fc0887ca63caf44978fc30d874c68610b5
cailun01 <1354213521@qq.com>
2021-06-14 18:15:45 +0800

    no

--- solutions/leetcode_204_solution_1.cc
+++ solutions/leetcode_204_solution_1.cc
@@ -1,7 +1,5 @@
-/*
+/* 204 计算质数
 统计所有小于非负整数 n 的质数的数量。
-
- 
 
 示例 1：
 
@@ -19,21 +17,27 @@
 */
 class Solution {
 public:
-    int countPrimes(int n) {
-        vector<bool> is_prime(n, true);
-        for (int i = 2; i * i < n; ++i) {
-            if (is_prime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    is_prime[j] = false;
-                }
-            }
-        }
-        int count = 0;
-        for (int i = 2; i < n; ++i) {
-            if (is_prime[i]) {
-                ++count;
-            }
-        }
-        return count;
+  int countPrimes(int n) {
+    if (n == 0 || n == 1) {
+      return 0;
     }
+    vector<bool> is_prime(n, true);
+    // 0和1都不是质数
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i < n; ++i) {
+      if (is_prime[i]) {
+        // i是质数，i的倍数不可能是质数了
+        for (int j = i * i; j < n; j += i) {
+          /* i=4时算法会标记4*2=8，4*3=12等等数字，
+          但是8和12已经被i = 2和i = 3的 2*4 和 3*4 标记过了。
+          所以直接从4*4开始标记
+          */
+          is_prime[j] = false;
+        }
+      }
+    }
+
+    int num_primes = std::count(is_prime.begin(), is_prime.end(), true);
+    return num_primes;
+  }
 };
