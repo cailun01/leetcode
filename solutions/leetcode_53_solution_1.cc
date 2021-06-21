1a2c67fb90a4059e901eca4fb713478183f1253b
cailun01 <1354213521@qq.com>
2021-06-21 17:37:47 +0800

    add notes

--- solutions/leetcode_53_solution_1.cc
+++ solutions/leetcode_53_solution_1.cc
@@ -11,26 +11,29 @@
 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
 */
 
+/*
+ 最大和只有两种情况：
+   1. 最大和的连续序列只有一个元素，即nums[i]本身
+   2. 最大和的连续序列有多个元素，即从前面某处nums[p](p<i)开始，
+      一直到nums[i]结尾。最大和是：
+      nums[p] + ... + nums[i-1] + nums[i] = dp[i - 1] + nums[i]
+*/
+
 class Solution {
 public:
-    int maxSubArray(vector<int> &nums) {
-      //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
-      int result = INT_MIN;
-      int numsSize = nums.size();
-      //dp[i]表示nums中以nums[i]结尾的最大子序和
-      vector<int> dp(numsSize);
-      dp[0] = nums[0];
-      result = dp[0];
-      for (int i = 1; i < numsSize; i++) {
-        // 最大和只有两种情况：
-        //   1. 最大和的连续序列只有一个元素，即nums[i]本身
-        //   2. 最大和的连续序列有多个元素，即从前面某处nums[p](p<i)开始，
-        //      一直到nums[i]结尾。最大和是：
-        //      nums[p] + ... + nums[i-1] + nums[i] = dp[i - 1] + nums[i]
-        dp[i] = max(dp[i - 1] + nums[i], nums[i]);
-        result = max(result, dp[i]);
-      }
-
-      return result;
+  int maxSubArray(vector<int> &nums) {
+    //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+    int result = INT_MIN;
+    int numsSize = nums.size();
+    //dp[i]表示nums中以nums[i]结尾的最大子序和
+    vector<int> dp(numsSize);
+    dp[0] = nums[0];
+    result = dp[0];
+    for (int i = 1; i < numsSize; i++) {
+      dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+      result = max(result, dp[i]);
     }
+
+    return result;
+  }
 };
