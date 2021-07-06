70781bbf02079fcaaaa5be0a04acc92ccba8a573
cailun01 <1354213521@qq.com>
2021-07-06 08:51:11 +0800

    add notes

--- solutions/leetcode_69_solution_2.cc
+++ solutions/leetcode_69_solution_2.cc
@@ -1,16 +1,19 @@
-/* 牛顿迭代法：
+/* 69 x的平方根
+牛顿迭代法：
 求p的平方根
 https://blog.csdn.net/yohjob/article/details/92024963
 xn+1 = xn - (xn^2 - p) / (2 * xn) = (xn^2 + p) / (2 * xn) = (xn + p / xn) / 2
 */
 class Solution {
 public:
-int mySqrt(int x){
-  if(x == 0 || x == 1) return x;
-  double res = 1;
-  while(fabs(res * res - x) > 0.1){
+  int mySqrt(int x) {
+    if(x == 0 || x == 1) { 
+      return x; 
+    }
+    double res = 1;
+    while(fabs(res * res - x) > 0.1){
       res = (res + x / res) / 2;
+    }
+    return int(res);
   }
-  return int(res);
- }
 };
