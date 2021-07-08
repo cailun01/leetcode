d76ec7ff0dd42dff2dac6b3ed8d6e32e05a00ed1
cailun01 <1354213521@qq.com>
2021-07-08 17:30:36 +0800

    add notes

--- solutions/leetcode_101_solution_1.cc
+++ solutions/leetcode_101_solution_1.cc
@@ -3,20 +3,21 @@
 
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        return isMirror(root, root);
+  bool isSymmetric(TreeNode* root) {
+      return isMirror(root, root);
+  }
+
+  bool isMirror(TreeNode* l, TreeNode* r) {
+    if (l == nullptr && r == nullptr) {
+      return true;
     }
-    bool isMirror(TreeNode* l, TreeNode* r) {
-        if (l == nullptr && r == nullptr) {
-            return true;
-        }
-        if (l == nullptr || r == nullptr) {
-            return false;
-        }
-        if (l->val == r->val) {
-            return isMirror(l->left, r->right) && isMirror(l->right, r->left);
-        } else {
-            return false;
-        }
+    if ((!l && r) || (l && !r)) {
+      return false;
     }
+    if (l->val == r->val) {
+      return isMirror(l->left, r->right) && isMirror(l->right, r->left);
+    } else {
+      return false;
+    }
+  }
 };
