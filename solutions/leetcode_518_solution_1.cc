6577945d23d9d92da40f84e5e1d1c4a7cc3b001f
cailun01 <1354213521@qq.com>
2021-07-15 23:38:39 +0800

    lc 518

--- solutions/leetcode_518_solution_1.cc
+++ solutions/leetcode_518_solution_1.cc
@@ -1,22 +1,32 @@
+/* 518. 零钱兑换 II
+给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+
+请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+
+假设每一种面额的硬币有无限个。 
+
+题目数据保证结果符合 32 位带符号整数。
+*/
+
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+  int change(int amount, vector<int>& coins) {
+    int n = coins.size();
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
 
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; ++j) {
-                if (j - coins[i - 1] >= 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        return dp[n][amount];
+    for (int i = 0; i <= n; i++) {
+      dp[i][0] = 1;
     }
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= amount; ++j) {
+        if (j - coins[i - 1] >= 0) {
+          dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+        } else {
+          dp[i][j] = dp[i - 1][j];
+        }
+      }
+    }
+    return dp[n][amount];
+  }
 };
